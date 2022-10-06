b79430ac2c385d62c92e43bac92af5865d9fe8aa
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-06 19:39:49 +0530

    Add files via upload
    Added UC1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Uc1PlayeratStartingPosition
+{
+    int START_POSITION = 0;
+    int END_POSITION = 100;
+    int playerPosition = 0;
+    int previousPlayerPosition = 0;
+    int updatedPlayerPosition = 0;
+    int countDiceRoll = 0;
+        static void Main(string[] args)
+        {
+    Console.WriteLine("Welcome to Snake And Ladder Game");
+        }
+}
---
+++ Uc1PlayeratStartingPosition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakandLadder.cs
+{
+    internal class Uc1PlayeratStartingPosition
+    { 
+        int START_POSITION = 0;
+        int END_POSITION = 100;
+        int playerPosition = 0;
+        int previousPlayerPosition = 0;
+        int updatedPlayerPosition = 0;
+        int countDiceRoll = 0;
+        //static void Main(string[]args)
+        static void playerposition()
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+        }
+    }
+}
