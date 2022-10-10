6bfb95bb0f6c88aa61707369a79764e0fd702e1e
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-10 21:19:52 +0530

    Add files via upload
    Added UC2

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UC2NumberBetween1_6.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            UC2Dice1_6.RollDice();
+        }
+    }
+}
---
+++ UC2Dice1-6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2NumberBetween1_6.cs
+{
+    internal class UC2Dice1_6
+    {
+        public static void RollDice()
+
+        {
+            int START_POSITION = 0;
+            int END_POSITION = 100;
+            int dieRolling;
+
+                Random random = new Random();
+                dieRolling = random.Next(1, 7);
+           Console.WriteLine("Number on die is: " + dieRolling);
+        }
+    }
+}
