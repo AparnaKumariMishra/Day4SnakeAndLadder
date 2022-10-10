2932294daffe2594ca47917c6b4b6ac26d5fca33
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-10 21:34:19 +0530

    Add files via upload
    Added Uc3

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserCase3CheckForOption
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            UC3CheckOption.winningPosition();
+        }
+    }
+}
---
+++ UC3CheckOption.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase3CheckForOption
+{
+    internal class UC3CheckOption
+    {
+        static int START_POSITION = 0;
+
+        static int WINNING_POSITION = 100;
+        public static void winningPosition()
+        {
+            Console.WriteLine("Welcome to snake ladder game");
+            Console.WriteLine("Player will start from zero position");
+
+                Random random1 = new Random();
+                int checkOptions = random1.Next(0, 3);
+                switch (checkOptions)
+                {
+
+                    case 0:
+                        {
+                            Console.WriteLine("There will be no play. Player will be at same position");
+                        }
+                        break;
+
+                    case 1:
+                    
+                            Console.WriteLine("Player will move forward");
+                          
+                        break;
+                    case 2:
+                       
+                            Console.WriteLine("Player is bitten by a snake");
+
+                        break;
+                    default:
+                    Console.WriteLine("start game again");
+                    break ;
+
+                }
+
+        
+            }
+        }
+    }
+
